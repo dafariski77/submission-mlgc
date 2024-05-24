77ce07e218e1dc640f7db1387efdc30fcafd58fb
dafariski77 <rskdafa@gmail.com>
2024-05-24 18:20:45 +0700

    feat: add firestore services

--- src/configs/index.js
+++ src/configs/index.js
@@ -2,5 +2,6 @@ require("dotenv").config();
 
 module.exports = {
   port: process.env.PORT ?? 5000,
-  modelUrl: process.env.MODEL_URL
+  modelUrl: process.env.MODEL_URL,
+  projectId: process.env.PROJECT_ID
 };
